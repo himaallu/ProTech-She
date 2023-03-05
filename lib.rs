f5b676197baf29a4d84f26716d7d534a70733bb1
himaallu <108365834+himaallu@users.noreply.github.com>
2023-03-05 16:03:48 +0400

    Add files via upload

--- lib.rs
+++ lib.rs
@@ -3,38 +3,41 @@ use anchor_lang::prelude::*;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-mod basic_1 {
+mod basic_2 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
-        let my_account = &mut ctx.accounts.my_account;
-        my_account.data = data;
+    pub fn create(ctx: Context<Create>, authority: Pubkey) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.authority = authority;
+        counter.count = 0;
         Ok(())
     }
 
-    pub fn update(ctx: Context<Update>, data: u64) -> Result<()> {
-        let my_account = &mut ctx.accounts.my_account;
-        my_account.data = data;
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 8)]
-    pub my_account: Account<'info, MyAccount>,
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 8 + 40)]
+    pub counter: Account<'info, Counter>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct Update<'info> {
-    #[account(mut)]
-    pub my_account: Account<'info, MyAccount>,
+pub struct Increment<'info> {
+    #[account(mut, has_one = authority)]
+    pub counter: Account<'info, Counter>,
+    pub authority: Signer<'info>,
 }
 
 #[account]
-pub struct MyAccount {
-    pub data: u64,
+pub struct Counter {
+    pub authority: Pubkey,
+    pub count: u64,
 }
